58802d15cd71306a54dc73354e2e845e8a138068
touhidpasha <touhidpasha552@gmail.com>
2021-09-30 19:01:30 +0530

    map-problems

--- rollDie.js
+++ rollDie.js
@@ -1,19 +1,17 @@
-// let dic=dictionary();
-// dic={1:0,2:0,3:0,4:0,5:0,6:0};
-// while(true){
-// n=Math.floor((Math.random() * 6) + 1);
-// dic[n]=dic[n].values()+1;
-// console.log(dic);
-// }
-let DiceValueMap=new Map();
-for (let [key,value] of DiceValueMap){
-	if(value == 10) {
-		winner = key;
+let dice={1:0,2:0,3:0,4:0,5:0,6:0};
+let flag=true;
+while(flag){
+	let n=Math.floor((Math.random() * 6) + 1);
+	dice[n]=dice[n]+1;
+	for (key in dice){
+		if(dice[key]>=10)
+		{console.log("MAXIMUM:dice number "+key+" got 10");
+		flag=false;
+		let min=0;
+		for (key in dice)
+		if(min>dice[key])
+		min=dice[key];
+		console.log("MINIMUM: dice number"+key+" got "+dice[key]+" occurence");
 	}
-	
-}
-while( count <= 10){
-    let key = Math.floor(Math.random() * 6) + 1;
-	DiceValueMap.set(key,count);
-    count++;
+	}
 }
